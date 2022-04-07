f41b9089d301d32aa06734636731ad30ea15b1a2
DrManganese <music.mathias@gmail.com>
2022-04-07 23:05:32 +0200

    bad test

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -8,4 +8,8 @@ class MainTest extends AnyFunSuite with Matchers {
   test("square should square") {
     Main.square(10) mustBe 100
   }
+  
+  test("square of 1 should always be 1") {
+    Main.square(1) mustBe 2
+  }
 }
